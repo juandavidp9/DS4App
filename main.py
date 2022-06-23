139b3a6b57fc657a1531077a23645886035e7008
juandavidp9 <65871593+juandavidp9@users.noreply.github.com>
2022-06-23 17:08:30 -0500

    Predictions
    Added RF model predictions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from operator import index
+import plotly.express as px
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -17,6 +18,7 @@ from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import ParameterGrid
 from sklearn.inspection import permutation_importance
+from shapely import wkb
 import multiprocessing
 warnings.filterwarnings('ignore')
 
@@ -24,6 +26,7 @@ st.set_page_config(layout="wide")
 header_container = st.container()
 
 @st.cache
+
 def get_data():
 	#We create the conecction with the database
 	conn = psycopg2.connect(host="data-prod.cem7ltlisydy.us-east-1.redshift.amazonaws.com", port = 5439, database="data-prod-co", user="food_user", password="F00do105y2o22")
@@ -33,23 +36,37 @@ def get_data():
 	#we get the dataframe
 	df = pd.DataFrame(orders_cooking_new)
 	df.reset_index()
-	df.columns = ['order_id', 'provider_orderid', 'kitchenid', 'kitchen','type','brand','provider_name', 'logcreatedat','check_orders','order_secs', 'num_items', 'total', 'polygon_name']
+	df.columns = ['order_id', 'provider_orderid', 'kitchenid', 'kitchen','type','brand','provider_name', 'logcreatedat','check_orders','minutes', 'num_items', 'total', 'polygon_name']
 	df['kitchenid'] = df['kitchenid'].astype('category')
 	df['kitchen'] = df['kitchen'].astype('category')
 	df['brand'] = df['brand'].astype('category')
 	df['num_items'] = df['num_items'].astype('float64')
 	df['total'] = df['total'].astype('float64')
 	df['provider_name'] = df['provider_name'].str.upper()
-	df['minutes'] = df['order_secs']/60 
-	df2 = df.copy()
-	df2 = df.drop(labels=['order_id', 'provider_orderid', 'kitchenid', 'order_secs', 'logcreatedat'], axis=1)
-	df2 = pd.get_dummies(df2, prefix =['kitchen', 'brand', 'provider_name', 'polygon_name'], drop_first=True)
-	df2 = df2.dropna()
+	df = df.drop(labels=['order_id', 'provider_orderid', 'kitchenid', 'logcreatedat'], axis=1)
+	return df
+
+def get_data2():
+	#We create the conecction with the database
+	conn = psycopg2.connect(host="data-prod.cem7ltlisydy.us-east-1.redshift.amazonaws.com", port = 5439, database="data-prod-co", user="food_user", password="F00do105y2o22")
+	cur = conn.cursor()
+	cur.execute("""SELECT * FROM cooking_time.orders_cooking_final""")
+	orders_cooking_new = cur.fetchall()
+	df2 = pd.DataFrame(orders_cooking_new)
+	df2.reset_index()
+	df2.columns = ['order_id', 'provider_orderid', 'kitchenid', 'kitchen','type','brand','provider_name', 'logcreatedat','check_orders','minutes', 'num_items', 'total', 'polygon_name', 'polygon_', 'point']
+	df2['polygon'] = df2['polygon_'][0:20].apply(lambda x : wkb.loads(x,hex=True))
+	df2 = df2.drop(columns=['provider_orderid','logcreatedat','check_orders', 'kitchen', 'polygon_', 'point'], axis=1)
 	return df2
 
- 
+
+
 def descriptive():
-	st.map()
+	df = get_data()
+	#st.text(df2.head(10))
+	fig = px.box(df, x="polygon_name", y='minutes')
+	c1, c2 = st.columns((1,1))
+	c1.plotly_chart(fig, use_container_width=True)
 
 def regression():
 	#st.write('You selected Linear Regression.')
@@ -60,6 +77,8 @@ def regression():
 
 def randomF():
 	df2 = get_data()
+	df2 = pd.get_dummies(df2, prefix =['kitchen', 'brand', 'provider_name', 'polygon_name'], drop_first=True)
+	df2 = df2.dropna()
 
 	c1, c2 = st.columns((1,1))	
 	c1.map()
@@ -70,7 +89,7 @@ def randomF():
 	
 	
 	X_train, X_test, y_train, y_test = train_test_split(
-                                        df2.drop(columns = ["minutes"]),
+                                        df2.drop(columns = ['minutes']),
                                         df2['minutes'],
                                         random_state = 123
                                     )
@@ -85,17 +104,27 @@ def randomF():
          )
 	model.fit(X_train, y_train)
 	pred = model.predict(X = X_test)
+	X_test['prediccion'] = pred
 
-	rmse = mean_squared_error(
+	rmse = round(mean_squared_error(
         y_true  = y_test,
         y_pred  = pred,
         squared = False
-       )
-	mae = mean_squared_error(y_test, pred)
-	r2 = r2_score(y_test, pred)
-	c2.metric(label="RMSE (Mean Squared Error)", value=rmse)
+       ), 2)
+	mae = round(mean_squared_error(y_test, pred),2)
+	r2 = round(r2_score(y_test, pred), 2)
+	c2.metric(label="RMSE (Root Mean Squared Error)", value=rmse)
 	c2.metric(label="R (R squared Score)", value=r2)
-	c2.metric(label="Mean Absolute Error", value=mae)
+	c2.metric(label="MAE (Mean Absolute Error)", value=mae)
+
+	Salitre = X_test[X_test['polygon_name_Salitre']==1]['prediccion'].mean()
+	Castellana = X_test[X_test['polygon_name_Castellana']==1]['prediccion'].mean()
+	Chapinero = X_test[X_test['polygon_name_Chapinero']==1]['prediccion'].mean()
+	Colina = X_test[X_test['polygon_name_Colina']==1]['prediccion'].mean()
+	Engativa = X_test[X_test['polygon_name_Engativa']==1]['prediccion'].mean()
+	Suba = X_test[X_test['polygon_name_Suba']==1]['prediccion'].mean()
+	Usaquen = X_test[X_test['polygon_name_Usaquen']==1]['prediccion'].mean()
+
 	
 
 
