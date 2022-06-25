dc85a7836f46f2a67239c189dd093746b88b0b21
juandavidp9 <65871593+juandavidp9@users.noreply.github.com>
2022-06-25 13:21:35 -0500

    Added df Polygon
    Added df Polygon

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import numpy as np
 import os
 import psycopg2
 from datetime import datetime, timedelta
+from scipy.stats import chi2_contingency
 import warnings
 #import statsmodels.api as sm
 #from tqdm import tqdm 
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 from sklearn.datasets import load_boston
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
@@ -20,6 +22,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import ParameterGrid
 from sklearn.inspection import permutation_importance
 from shapely import wkb
+import requests
+from io import StringIO
+from io import BytesIO
 import multiprocessing
 warnings.filterwarnings('ignore')
 
@@ -30,12 +35,18 @@ header_container = st.container()
 
 def get_data():
 	#We create the conecction with the database
-	conn = psycopg2.connect(host="data-prod.cem7ltlisydy.us-east-1.redshift.amazonaws.com", port = 5439, database="data-prod-co", user="food_user", password="F00do105y2o22")
-	cur = conn.cursor()
-	cur.execute("""SELECT * FROM cooking_time.orders_cooking_new""")
-	orders_cooking_new = cur.fetchall()
+	#conn = psycopg2.connect(host="data-prod.cem7ltlisydy.us-east-1.redshift.amazonaws.com", port = 5439, database="data-prod-co", user="food_user", password="F00do105y2o22")
+	#cur = conn.cursor()
+	#cur.execute("""SELECT * FROM cooking_time.orders_cooking_new""")
+	#orders_cooking_new = cur.fetchall()
 	#we get the dataframe
-	df = pd.DataFrame(orders_cooking_new)
+	#df = pd.DataFrame(orders_cooking_new)
+	original_url = "https://drive.google.com/file/d/1mK15-Qmk5vYdq91hAuLpdNeLOKfGemdt/view?usp=sharing"
+	file_id = original_url.split('/')[-2]
+	dwn_url='https://drive.google.com/uc?export=download&id=' + file_id
+	url = requests.get(dwn_url).text
+	path = StringIO(url)
+	df = pd.read_csv(path)
 	df.reset_index()
 	df.columns = ['order_id', 'provider_orderid', 'kitchenid', 'kitchen','type','brand','provider_name', 'logcreatedat','check_orders','minutes', 'num_items', 'total', 'polygon_name']
 	df['kitchenid'] = df['kitchenid'].astype('category')
@@ -49,15 +60,23 @@ def get_data():
 
 def get_data2():
 	#We create the conecction with the database
-	conn = psycopg2.connect(host="data-prod.cem7ltlisydy.us-east-1.redshift.amazonaws.com", port = 5439, database="data-prod-co", user="food_user", password="F00do105y2o22")
-	cur = conn.cursor()
-	cur.execute("""SELECT * FROM cooking_time.orders_cooking_final""")
-	orders_cooking_new = cur.fetchall()
-	df2 = pd.DataFrame(orders_cooking_new)
+	#conn = psycopg2.connect(host="data-prod.cem7ltlisydy.us-east-1.redshift.amazonaws.com", port = 5439, database="data-prod-co", user="food_user", password="F00do105y2o22")
+	#cur = conn.cursor()
+	#cur.execute("""SELECT * FROM cooking_time.orders_cooking_final""")
+	#orders_cooking_new = cur.fetchall()
+	#df2 = pd.DataFrame(orders_cooking_new)
+	#df2.reset_index()
+	#df2.columns = ['order_id', 'provider_orderid', 'kitchenid', 'kitchen','type','brand','provider_name', 'logcreatedat','check_orders','minutes', 'num_items', 'total', 'polygon_name', 'polygon_', 'point']
+	#df2['polygon'] = df2['polygon_'][0:100].apply(lambda x : wkb.loads(x,hex=True))
+	#df2 = df2.drop(columns=['provider_orderid','logcreatedat','check_orders', 'kitchen', 'polygon_', 'point'], axis=1)
+	original_url = "https://drive.google.com/file/d/1dX3AJxJx2r5oqlRwhv3O6WBnCgYgCv5l/view?usp=sharing"
+	file_id = original_url.split('/')[-2]
+	dwn_url='https://drive.google.com/uc?export=download&id=' + file_id
+	url = requests.get(dwn_url).text
+	path = StringIO(url)
+	df2 = pd.read_csv(path)
 	df2.reset_index()
-	df2.columns = ['order_id', 'provider_orderid', 'kitchenid', 'kitchen','type','brand','provider_name', 'logcreatedat','check_orders','minutes', 'num_items', 'total', 'polygon_name', 'polygon_', 'point']
-	df2['polygon'] = df2['polygon_'][0:100].apply(lambda x : wkb.loads(x,hex=True))
-	df2 = df2.drop(columns=['provider_orderid','logcreatedat','check_orders', 'kitchen', 'polygon_', 'point'], axis=1)
+	df2.columns = ['polygon_name', 'polygon', 'point']
 	return df2
 
 
@@ -68,12 +87,24 @@ def descriptive():
 	fig = px.box(df, x="polygon_name", y='minutes')
 	fig2 = px.scatter(df, x='total', y='minutes').update_traces(marker=dict(color='#2ca02c'))
 	fig4 = px.scatter(df, x='logcreatedat', y="minutes")
-	fig3 = px.scatter(df, x='num_items', y='total').update_traces(marker=dict(color='#17becf'))
+	fig3 = px.scatter(df, x='num_items', y='minutes').update_traces(marker=dict(color='#17becf'))
+	subset = pd.crosstab(df["brand"], df["kitchen"])
+	def df_to_plotly(subset):
+		return {'z': subset.values.tolist(),
+		        'x': subset.columns.tolist(),
+                'y': subset.index.tolist()}
+	fig5 = go.Figure(data=go.Heatmap(df_to_plotly(subset)))
+	p = chi2_contingency(subset)[1]
+
 	c1, c2 = st.columns((1,1))
 	c1.plotly_chart(fig, use_container_width=True)
 	c2.plotly_chart(fig2, use_container_width=True)
 	c1.plotly_chart(fig3, use_container_width=True)
 	c2.plotly_chart(fig4, use_container_width=True)
+	c1.plotly_chart(fig5, use_container_width=True)
+	#c2.text("p-value of Chi-square test for Brand vs. Kitchen=", p)
+	c1.markdown('**P-value of Chi-square test for Brand vs. Kitchen = 0.0**')
+	#c1.markdown("<h1 style='text-align: center>**p-value of Chi-square test for Brand vs. Kitchen=0.0** </h1>", unsafe_allow_html=True)
 
 def regression():
 	#st.write('You selected Linear Regression.')
@@ -131,6 +162,11 @@ def randomF():
 	Engativa = X_test[X_test['polygon_name_Engativa']==1]['prediccion'].mean()
 	Suba = X_test[X_test['polygon_name_Suba']==1]['prediccion'].mean()
 	Usaquen = X_test[X_test['polygon_name_Usaquen']==1]['prediccion'].mean()
+	predicciones = [Chapinero, Castellana, Colina, Salitre, Suba, 20.306155826861165, Usaquen]
+
+	df3 = get_data2()
+	df3.insert(loc=3, column='prediction', value=predicciones)
+
 
 	
 
